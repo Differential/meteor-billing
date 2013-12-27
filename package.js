41b31177d34f45e370d8d173eabde28995581d22
schnie <greg@differential.io>
2013-12-27 13:49:04 -0500

    initial commit -cc view

---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  summary: "Various billing functionality packaged up"
+});
+
+Package.on_use(function (api, where) {
+  api.use([
+    'templating',
+    'handlebars',
+    'less'
+  ], 'client');
+
+  api.add_files([
+    'client/creditCard/creditCard.html',
+    'client/creditCard/creditCard.less'
+  ], ['client']);
+});
