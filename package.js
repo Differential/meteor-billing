f57954a79a40874f4ce3ec1a482c19808db65366
mathew <leaderofthepack@discokid.local>
2014-10-01 15:47:51 -0400

    version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "ryw:billing",
   summary: "Various billing functionality packaged up.",
-  version: "2.0.0",
+  version: "2.0.1",
   git: "https://github.com/Differential/meteor-billing"
 });
 
